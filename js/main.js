055e4b14fcb19ac1387a671372665739e368ad73
Mykola Kolomoyets <53149094+mykola-kolomoyets@users.noreply.github.com>
2020-07-10 11:44:00 +0300

    v1.0
    - Added the start screen block
    - Added sliders with slick.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+	$(".fairy-tail__slider, .our-trip__slider").slick({
+		prevArrow: '<button class=" slick-btn slick-prev"><img src="./images/arrow-prev.svg"></button>',
+		nextArrow: '<button class=" slick-btn slick-next"><img src="./images/arrow-next.svg"></button>',
+		autoplay: 3000,
+		fade: true
+	});
+});
