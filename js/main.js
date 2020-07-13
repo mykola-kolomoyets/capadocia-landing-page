2120868ff480ffd57db5bf277c9e19fa8bf6788f
Mykola Kolomoyets <53149094+mykola-kolomoyets@users.noreply.github.com>
2020-07-13 21:08:40 +0300

    v1.5
    - Added adaptivity (flexbox and media-queries)

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(function(){
 		prevArrow: '<button class=" slick-btn slick-prev"><img src="./images/arrow-prev.svg"></button>',
 		nextArrow: '<button class=" slick-btn slick-next"><img src="./images/arrow-next.svg"></button>',
 		autoplay: 3000,
-		fade: true
+		fade: true,
+		responsive: [{
+			breakpoint: 601,
+			settings: {
+				arrows: false
+			}
+		}]
 	});
 });
